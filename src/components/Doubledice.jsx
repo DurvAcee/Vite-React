646de6dee6186a39017c8c3b55841cc1e8276fb2
Durvesh Danve <Durvesh.Danve@veritas.com>
2024-06-12 15:35:05 +0530

    Add Dynamic styles

--- src/components/Doubledice.jsx
+++ src/components/Doubledice.jsx
@@ -3,10 +3,13 @@ export default function Doubledice() {
     const num1 = Math.floor(Math.random() * 6) + 1;
     const num2 = Math.floor(Math.random() * 6) + 1;
 
+    const isWinner = num1 === num2;
+    const styles = {color: isWinner? 'green' : 'red'};
+
     return (
         <>
-            <h2>Double Dice</h2>
-            {num1 === num2 && <h3> You Win! </h3>}
+            <h2 style = {styles}>Double Dice</h2>
+            {isWinner && <h3> You Win! </h3>}
             <p>num1 : {num1}</p>
             <p>num1 : {num2}</p>
         </>
